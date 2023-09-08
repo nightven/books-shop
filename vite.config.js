e99707493a4f4afbbfaacba9a877a1cb7ff7ff29
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-09-08 12:58:53 +0200

    new init

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
-// vite.config.js
 import { defineConfig } from 'vite';
-import { glob } from 'glob';
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-	root: 'src',
-	build: {
-	  rollupOptions: {
-		input: glob.sync('./src/*.html'),
-	  },
-	  outDir: '../dist',
-	},
-	base: "/books-shop/",
-  plugins: [injectHTML(), FullReload(['./src/**/**.html']), ghPages()],
+  root: 'src',
+  build: {
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+    },
+    outDir: '../dist',
+  },
+  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
 });
